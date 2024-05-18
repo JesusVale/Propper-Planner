d1f6ee21fcdb45b090aef91c6321d4babc3f014b
Jesus Gabriel Valencia Camacho <jvalencia@correo.com>
2024-05-18 13:07:08 -0700

    Create Constants

---
+++ src/constants.ts
@@ -0,0 +1,24 @@
+export const MONTHS = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+]
+
+export const WEEK_DAYS = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado"
+]
