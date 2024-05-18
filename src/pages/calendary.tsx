cbaed4277d1b7d6a1b4e85152df158760b04cd1c
Jesus Gabriel Valencia Camacho <jvalencia@correo.com>
2024-05-18 13:14:41 -0700

    Create Calendary Page

---
+++ src/pages/calendary.tsx
@@ -0,0 +1,38 @@
+import useCalendar from "../hooks/useCalendar";
+import { RightArrow, LeftArrow } from "../components/icons";
+import CalendarMonth from "../components/calendar/CalendarMonth";
+import { SelectedDayProvider } from "../context/selectedDay";
+
+function Calendary() {
+
+  const { addOne, removeOne, getStringDate, daysInMonth, loading } = useCalendar();
+
+  return (
+    <div className="mx-auto w-9/12">
+      <div className="flex justify-between">
+        <button onClick={removeOne} className="bg-main-color rounded-[50%] w-12 h-12 flex justify-center items-center">
+          <LeftArrow color="#fff" width={30} height={30} />
+        </button>
+        <h1 className="text-center my-2 font-title-font text-3xl font-semibold">{getStringDate()}</h1>
+        <button onClick={addOne} className="bg-main-color rounded-[50%] w-12 h-12 flex justify-center items-center">
+          <RightArrow color="#fff" width={25} height={25} />
+        </button>
+      </div>
+      {
+        loading ? 
+        <div>Cargando...</div>
+        :
+        <SelectedDayProvider>
+          <>   
+            <CalendarMonth daysMonth={daysInMonth} />
+          </>
+        </SelectedDayProvider>
+      }
+      
+      
+    </div>
+    
+  )
+}
+
+export default Calendary
