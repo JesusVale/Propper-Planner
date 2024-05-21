6895ac71aeff0bfb45da4dd2786eb34b559d87f1
Jesus Gabriel Valencia Camacho <jvalencia@correo.com>
2024-05-21 13:25:34 -0700

    Add TimeIcon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -110,3 +110,16 @@ export function DeleteIcon({width, height, color="#000"}: BasicIconProps){
     )
     
 }
+
+export function TimeIcon({width, height}: BasicIconProps){
+    return (
+
+        <svg  xmlns="http://www.w3.org/2000/svg"  width={width}  height={height}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-clock-hour-4">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            <path d="M12 12l3 2" />
+            <path d="M12 7v5" />
+        </svg>
+
+    )
+}
